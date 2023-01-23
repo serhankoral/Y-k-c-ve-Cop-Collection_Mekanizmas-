067fab706869369bc67fb4ab668b45b9543de567
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-23 16:16:11 +0300

    Proje dosyası ekle.

---
+++ Yıkıcı ve Cop Collection_Mekanizması/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Yıkıcı_ve_Cop_Collection_Mekanizması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyClass myClass = new MyClass();
+            new MyClass();//Bu Şekilde nesneye bağlamadan işlem yapıldığında garben collecter temizlik yapar
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+    class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("MyClass oluştu");
+        }
+        ~MyClass()
+        {
+            Console.WriteLine("MyClass Silindi");
+        }
+
+        public void yaz()
+        {
+            Console.WriteLine("Yaz");
+        }
+    }
+}
